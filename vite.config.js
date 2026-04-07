598d8a494b7b2c5ac4af21e55c22473a83b03759
malati1 <malatisahu@m.kiot.io>
2026-04-07 14:32:23 +0530

    css updated

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].[hash].js',
-        chunkFileNames: '[name].[hash].js',
-        assetFileNames: '[name].[hash].[ext]'
-      }
-    }
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
   }
 })
